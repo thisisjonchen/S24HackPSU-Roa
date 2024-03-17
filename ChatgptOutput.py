567d08b7f7cabe6d4dbd88daa438809189d3d5e6
Matt-Witmer <152342009+Matt-Witmer@users.noreply.github.com>
2024-03-17 10:00:32 -0400

    Update ChatgptOutput.py

--- ChatgptOutput.py
+++ ChatgptOutput.py
@@ -5,21 +5,27 @@
 from openai import OpenAI
 client = OpenAI(api_key = "API_KEY_HERE")
 
-our_output = 100 #our confidence
-their_output = 100 #their confidence
-message = "You have to write a message to the driver of a car telling them to be careful due to "
+our_output = ["too windy, thunderstorm"] #our confidence
+their_output = 10 #their confidence
+message = "write a message with confidence of "
+
 if isinstance(our_output,list):
     confidence = (100+their_output)/2.0
+    message+= str(confidence)+" with the reason being due to "
     for reason in our_output:
         message+=str(reason)+", "
+
 else:
     confidence = (our_output+their_output)/2.0
-    message+="potentially dangerous road situation "
+    message+=str(confidence)
 
-message +="but with only a "+str(confidence) + " percent urgency. It can only be a few words and does not have to be proper grammer." 
 if(confidence>0):
     chat_completion = client.chat.completions.create(
         messages=[
+            {
+                "role": "system",
+                "content": "You are writing a message to someone currently driving a car to alert the driver that the road situation they are in looks like a situation in which a crash has either happened or is likely to happen.  The message should only be a few words long and does not need to have proper grammar. I will provide a confidence level for your response on a scale of 0-100. A response with higher confidence should be more assertive and urgent, while a message with low confidence should be a weaker, less stressed recommendation. An example of a message with high confidence is 'URGENT, DRIVE SAFE' while an example for a message with low confidence is 'caution.'"
+            },
             {
                 "role": "user",
                 "content": message,
@@ -40,5 +46,5 @@ if(confidence>0):
 
 #response.stream_to_file(speech_file_path)
 
-print(confidence)
-print(message)
+#print(confidence)
+#print(message)
