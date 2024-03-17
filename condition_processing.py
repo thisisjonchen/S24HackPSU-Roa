b718f58b38173e5600629c3dce5115b843519770
Vinh Ngo <117776860+vinhngo380@users.noreply.github.com>
2024-03-17 01:38:23 -0400

    Update condition_processing.py

--- condition_processing.py
+++ condition_processing.py
@@ -13,7 +13,7 @@ road_temp = 280 #hardcoded number in Kelvin to represent
 is_black_ice = 0 #0 if little to no chance of black ice, 16 if chance of black ice on road
 alert_level = 3 #number from 0-4 representing the severiety of possible weather condidtions (thunderstorm/tornado)
 wind_speed =  6 #number from 0 to about 15(max) of the current wind speed in m/s
-precipitation_rate = 51 #precipitation in the area in mm/hr
+precipitation_rate = 21 #precipitation in the area in mm/hr
 speed_limit = 45 #speed limit of the road the car is driving on
 alert_name = "" # a string which represents any possible weather condition
 #for getting whether or not the user is in a city
@@ -71,58 +71,3 @@ def get_speed_limit(location: tuple) -> float:
           wind_speed: {wind_speed}
     """)
     
-
-FREEZING_TEMP_KELVIN = 273.5
-BLACK_ICE_STATUS = 16
-DANGER_WIND_SPEED_METERS_SECOND = 13.4
-HIGH_WIND_SPEED_METERS_SECOND = 10
-VIOLENT_RAIN_RATE_MM_H = 50
-HEAVY_RAIN_RATE_MM_H = 7.6
-MODERATE_RAIN_RATE_MM_H = 2.5 
-LIGHT_RAIN_RATE_MM_H = 0.1
-#road_temp, is_black_ice, alert_level, wind_speed, precipitation_rate, speed_limit, alert_name
-def is_dangerous():
-    """
-    returns either a List (flags) or float (confidence meter)
-    flags is used whenever weather is bad enough so that the user should take immediate caution (i.e when black ice occurs)
-    a positive confidence level indicates that conditions can be potentially unsafe
-    a negative confidence level indicates that conditions are more safe
-    """
-    total_risk = 0 #total value of rankings from conditions
-    flags = [] #collection of reasons why the condition was automatically flagged
-    if road_temp<FREEZING_TEMP_KELVIN: #if road temp if below freezing, add to total risk based on decision matrix in documentation
-        total_risk+=2
-
-    if is_black_ice == BLACK_ICE_STATUS: 
-
-        flags.append("Icy roads")
-    if alert_level == 4:
-        flags.append(alert_name)
-    else:
-        total_risk+=5*(alert_level/3.0)
-
-    if wind_speed > DANGER_WIND_SPEED_METERS_SECOND:
-        flags.append("Too windy")
-        
-    elif wind_speed > HIGH_WIND_SPEED_METERS_SECOND:
-        total_risk+=2
-    if precipitation_rate > VIOLENT_RAIN_RATE_MM_H:
-        flags.append("Violent rain")
-    elif precipitation_rate > HEAVY_RAIN_RATE_MM_H:
-        total_risk+=8
-    elif precipitation_rate > MODERATE_RAIN_RATE_MM_H:
-        total_risk+=4
-    elif precipitation_rate > LIGHT_RAIN_RATE_MM_H:
-        total_risk+=2
-
-    speed_multiplier= speed_limit/35.0
-    total_risk = total_risk*speed_multiplier
-    confidence = min(abs((total_risk-8.5)/8.5)*100,100)
-
-    if len(flags)>0:
-        return flags
-
-    return confidence
-
-if __name__ == '__main__':
-    pass
