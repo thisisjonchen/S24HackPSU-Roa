2b9ef51d6bab1b2a9fc4a623105136f6c4e57926
Matt-Witmer <152342009+Matt-Witmer@users.noreply.github.com>
2024-03-17 11:33:32 -0400

    Update test_conditions.py

--- test_conditions.py
+++ test_conditions.py
@@ -11,6 +11,86 @@ if __name__ == '__main__':
     speed_limit = 45 #speed limit of the road the car is driving on
     alert_name = "" # a string which represents any possible weather condition
     #for getting whether or not the user is in a city
+    print("Given the condidtions: \nRoad temp is above freezing \nNo black ice \nHeavy rain \nSpeed limit: 45 \n we get a score of: ",end="")
+    print(is_dangerous.is_dangerous(road_temp, 
+                                            is_black_ice,
+                                            alert_level,
+                                            alert_name, 
+                                            wind_speed, 
+                                            precipitation_rate, 
+                                            speed_limit))
+    print()
+    is_black_ice=16
+    print("If we then declare black ice is on the road, we get: ",end="")
+    print(is_dangerous.is_dangerous(road_temp, 
+                                            is_black_ice,
+                                            alert_level,
+                                            alert_name, 
+                                            wind_speed, 
+                                            precipitation_rate, 
+                                            speed_limit))
+    print()
+    speed_limit = 25
+    print("If we then decrease the speed limit to 25mph, we get: ",end="")
+    print(is_dangerous.is_dangerous(road_temp, 
+                                            is_black_ice,
+                                            alert_level,
+                                            alert_name, 
+                                            wind_speed, 
+                                            precipitation_rate, 
+                                            speed_limit))
+    print()
+    road_temp = 0
+    alert_level = 4
+    alert_name = "Earth froze over"
+    wind_speed = 13.5
+    precipitation_rate = 61
+    speed_limit = 35
+    print("If the earth freezes over and there's a massive blizzard, we get: ",end="")
+    print(is_dangerous.is_dangerous(road_temp, 
+                                            is_black_ice,
+                                            alert_level,
+                                            alert_name, 
+                                            wind_speed, 
+                                            precipitation_rate, 
+                                            speed_limit))
+    print()
+    road_temp = 280
+    alert_level = 0
+    alert_name = ""
+    wind_speed=0
+    precipitation_rate = 0
+    speed_limit = 35
+    print("When the earth melts back and the weather is clear, we get: ",end="")
+    print(is_dangerous.is_dangerous(road_temp, 
+                                            is_black_ice,
+                                            alert_level,
+                                            alert_name, 
+                                            wind_speed, 
+                                            precipitation_rate, 
+                                            speed_limit))
+    print()
+    precipitation_rate = 1
+    print("As we increase the level of rainfall to fall within the 'Light Rain' category, confidence falls to: ",end="")
+    print(is_dangerous.is_dangerous(road_temp, 
+                                            is_black_ice,
+                                            alert_level,
+                                            alert_name, 
+                                            wind_speed, 
+                                            precipitation_rate, 
+                                            speed_limit))
+    print()
+    precipitaion_rate = 3
+    print("Then to 'Moderate Rain', confidence falls to: ",end="")
+    print(is_dangerous.is_dangerous(road_temp, 
+                                            is_black_ice,
+                                            alert_level,
+                                            alert_name, 
+                                            wind_speed, 
+                                            precipitation_rate, 
+                                            speed_limit))
+    print()
+    print("Then to 'Heavy Rain', confidence falls to: ",end="")
     print(is_dangerous.is_dangerous(road_temp, 
                                             is_black_ice,
                                             alert_level,
