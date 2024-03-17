fb85529f43094865157841d6875af91bc27e0c80
Matt-Witmer <152342009+Matt-Witmer@users.noreply.github.com>
2024-03-17 01:24:22 -0400

    Update ChatgptOutput.py

--- ChatgptOutput.py
+++ ChatgptOutput.py
@@ -1,13 +1,12 @@
 #python -m venv openai-env
 #openai-env\Scripts\activate
 #pip install --upgrade openai
-#python ChatgptOutput.py
 
 from openai import OpenAI
-client = OpenAI(api_key = "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")
+client = OpenAI(api_key = "API_KEY_HERE")
 
-our_output = ["too Windy","Violent Rain", "Hail", "Tornado"] #our confidence
-their_output = -80 #their confidence
+our_output = 100 #our confidence
+their_output = 100 #their confidence
 message = "You have to write a message to the driver of a car telling them to be careful due to "
 if isinstance(our_output,list):
     confidence = (100+their_output)/2.0
@@ -17,16 +16,18 @@ else:
     confidence = (our_output+their_output)/2.0
     message+="potentially dangerous road situation "
 
-message +="but you are "+str(confidence) + " percent confident. It can only be a few words and does not have to be proper grammer. Be super passive agressive" 
-chat_completion = client.chat.completions.create(
-    messages=[
-        {
-            "role": "user",
-            "content": message,
-        }
-    ],
-    model="gpt-3.5-turbo",
-)
+message +="but with only a "+str(confidence) + " percent urgency. It can only be a few words and does not have to be proper grammer." 
+if(confidence>0):
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": message,
+            }
+        ],
+        model="gpt-3.5-turbo",
+    )
+    print(chat_completion.choices[0].message.content)
 #from pathlib import Path
 #from openai import OpenAI
 
@@ -39,7 +40,5 @@ chat_completion = client.chat.completions.create(
 
 #response.stream_to_file(speech_file_path)
 
-
-print(chat_completion.choices[0].message.content)
-#print(confidence)
-#print(message)
+print(confidence)
+print(message)
